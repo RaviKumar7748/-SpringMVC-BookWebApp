1a0915b74ef7c82ab6879e8afd8f4f86be76ca8b
DELL <DELL@DESKTOP-SPEQPAE>
2022-10-21 00:05:29 +0530

    Added mew api for test hello msg

--- src/main/java/com/bookstore/controller/BookController.java
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -86,4 +88,8 @@ public class BookController {
 		return "viewBooks";
 	}
 
+	@PostMapping("/test/{msg}")
+	public String testHello(@PathVariable String msg) {
+		return msg;
+	}
 }
